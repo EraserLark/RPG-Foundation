cf731f69198916390f68635f727f04510eff77c4
Conner A <eraserlark@gmail.com>
2024-07-28 18:30:38 -0500

    Overworld demo changes

--- Overworld/OW_Actor.gd
+++ Overworld/OW_Actor.gd
@@ -4,7 +4,7 @@ class_name OW_Actor
 signal send_message(name:String, message:String)
 
 func interactAction(interacter : OW_Player):
-	var message = "[b]I[/b] [i]think[/i] [u]I[/u] [s]had[/s] [font_size=30]some[/font_size] [b][i]bad[/i][/b] [color=yellow]tofu [bgcolor=red]earlier[/bgcolor][/color]. [wave]I'm feeling [rainbow]fruity[/rainbow].[/wave]"
+	var message = "Don't talk to that snowman next to me. That guy is [shake]PISSED[/shake]"
 	speak(message)
 
 func speak(message : String):
