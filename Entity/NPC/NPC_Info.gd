20e00f1e93fe0e879ca671c75b1a543f7457bcc2
Conner A <eraserlark@gmail.com>
2024-08-10 14:00:07 -0500

    NPC Info changes update in editor

--- Entity/NPC/NPC_Info.gd
+++ Entity/NPC/NPC_Info.gd
@@ -1,14 +1,62 @@
+@tool
 extends Resource
 class_name NPC_Info
 
+var ow_npc: OW_Actor
+
 @export var npcName: String
 
+@export_group("Sprite Properties")
+@export var spriteSheet: Texture: set = setSpriteTexture
+@export var spriteOffset: Vector2: set = setSpriteOffset
+@export var hFrames: int: set = setHFrames
+@export var vFrames: int: set = setVFrames
+@export var currentFrame: int: set = setCurrentFrame
+
+@export_group("UI Properties")
 @export var characterPortraits: Array[Texture]
 @export var font: Font
 @export var typeAudio: AudioStream
 
-@export var spriteSheet: Texture
-@export var hFrames: int
-@export var vFrames: int
+@export_group("Other Properties")
+@export var collisionShape: Shape2D: set = setCollisionShape
+@export var speechSpotPos: Vector2: set = setSpeechSpot
 
-@export var collisionShape: Shape2D
+func setSpriteTexture(texture: Texture):
+	spriteSheet = texture
+	if(ow_npc):
+		ow_npc.get_node("Sprite2D").texture = spriteSheet
+
+func setSpriteOffset(offset: Vector2):
+	spriteOffset = offset
+	if(ow_npc):
+		ow_npc.get_node("Sprite2D").offset = spriteOffset
+
+func setHFrames(hf: int):
+	if(hf <= 0):
+		hf = 1
+	hFrames = hf
+	if(ow_npc):
+		ow_npc.get_node("Sprite2D").hframes = hFrames
+
+func setVFrames(vf: int):
+	if(vf <= 0):
+		vf = 1
+	vFrames = vf
+	if(ow_npc):
+		ow_npc.get_node("Sprite2D").vframes = vFrames
+
+func setCurrentFrame(cf: int):
+	currentFrame = cf
+	if(ow_npc):
+		ow_npc.get_node("Sprite2D").frame = currentFrame
+
+func setCollisionShape(shape: Shape2D):
+	collisionShape = shape
+	if(ow_npc):
+		ow_npc.get_node("CollisionShape2D").shape = collisionShape
+
+func setSpeechSpot(pos: Vector2):
+	speechSpotPos = pos
+	if(ow_npc):
+		ow_npc.get_node("SpeechSpot").position = speechSpotPos
--- Overworld/Cast/OW_Actor.gd
+++ Overworld/Cast/OW_Actor.gd
@@ -1,13 +1,27 @@
+@tool
 extends CharacterBody2D
 class_name OW_Actor
 
 var dbcPath = "res://UI/DialogueBubbleContainer.tscn"
 
-@onready var ui:= $"../../../../CanvasLayer/OW_UI"
+#@onready var ui:= $"../../../../CanvasLayer/OW_UI" - Uncomment
 @onready var speechSpot:= $SpeechSpot
 
-#@export_multiline var message: Array[String]
+@export var npcResource: NPC_Info: set = setNPCInfo
 @export var cutsceneResource: PackedScene
+
+func setNPCInfo(info: NPC_Info):
+	npcResource = info
+	info.ow_npc = self
+	
+	self.name = info.npcName
+	$Sprite2D.texture = info.spriteSheet
+	$Sprite2D.offset = info.spriteOffset
+	$Sprite2D.hframes = info.hFrames
+	$Sprite2D.vframes = info.vFrames
+	$Sprite2D.frame = info.currentFrame
+	$CollisionShape2D.shape = info.collisionShape
+	$SpeechSpot.position = info.speechSpotPos
 
 func interactAction(interacter : OW_Player):
 	#speak(message)
@@ -20,7 +34,7 @@ func createDBC():
 	#Create dialogueBubbleContainer
 	var dbc = load(dbcPath)
 	var inst = dbc.instantiate()
-	ui.add_child(inst)
+	#ui.add_child(inst) -- Uncomment
 	#Assign its transform to the SpeechSpot
 	inst.refSpot = speechSpot
 	return inst
