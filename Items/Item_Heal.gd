ce4cd7e4ebbeb8d234a6efe5500da0eda8b08fbc
Conner A <eraserlark@gmail.com>
2024-09-10 10:30:26 -0500

    Change Heal Item name

--- Items/Item_Heal.gd
+++ Items/Item_Heal.gd
@@ -2,14 +2,14 @@ extends Item
 class_name Item_Heal
 
 func _init():
-	super("Mushroom")
+	super("Health Pack")
 	itemAction = HealAction.new(null, null, null, Action.TargetTypes.PLAYER, null)
 
 class HealAction:
 	extends Action
 	func _init(eManager, send, targ, targType, stgmn: StageManager):
 		super(eManager, send, targ, targType, stgmn)
-		eventName = "Mushroom"
+		eventName = "Health Pack"
 	func runEvent():
 		target = sender
 		target.reactionComplete.connect(resumeEvent)
