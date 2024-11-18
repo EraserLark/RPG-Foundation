15f588d040fc5c9d7f2dd14cba545ff9c55e68df
Alex Hanlin <gluethulhu@gmail.com>
2024-11-17 21:34:29 -0600

    Game scene as singleton that grabs loaded room on startup

--- Overworld/OverworldManager.gd
+++ Overworld/OverworldManager.gd
@@ -17,13 +17,28 @@ class_name OverworldManager
 func _ready():
 	print("Overworld Ready Start")
 	super()
+
+	var start_room: Room = find_start_room()
+
+	# wait for all other nodes to be ready, then continue
+	await $/root.ready
 	
 	stageUI = $CanvasLayer/OW_UI
 	
 	stageUI.initialize(self)
-	overworldWorld.initialize(self)
+	overworldWorld.initialize(self, start_room)
 	cutsceneManager.stageManager = self
 	
 	var overworldGameState = GameState_Overworld.new()	#Adds itself to GameStateStack
 	
 	print("Overworld Ready Finish")
+
+
+# Scan the root-level nodes to see if the game has a valid room.
+func find_start_room() -> Room:
+	for node: Node in $/root.get_children():
+		if node is Room:
+			return node
+
+	printerr("Game started in a scene that is not a room! Can't initialize global info.")
+	return null
