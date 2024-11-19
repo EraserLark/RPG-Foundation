420c186edb05e7afbca36e6bc41a5a9ad76f2eae
Alex Hanlin <gluethulhu@gmail.com>
2024-11-19 13:55:23 -0600

    Basic Debug Menu With Tabs and Game State

---
+++ Debug/DebugGameStackDisplay.gd
@@ -0,0 +1,30 @@
+extends Control
+class_name DebugGameStackDisplay
+
+@onready var text: Label = $StackText
+
+var stackCount: int = -1;
+var topState: GameState = null;
+
+# Inefficient, but should work. Can replace with signal listeners when appropriate
+func _process(_delta):
+	_update_data()
+
+func _update_data() -> void:
+	var stack := GameStateStack.gameStateStack
+	var stackChanged: bool = (stack.size() != stackCount || stack.front() != topState)
+	if (!stackChanged): return
+
+	var displayString: String = ""
+
+	for state in stack:
+		displayString += _get_state_name(state) + '\n'
+	displayString = displayString.trim_suffix('\n') # get rid of trailing line return
+
+	text.text = displayString
+	
+	
+func _get_state_name(state: GameState) -> String:
+	var filename : String = state.get_script().resource_path.get_file()
+	return filename.trim_suffix(".gd") # remove script suffix
+	
---
+++ Debug/DebugManager.gd
@@ -0,0 +1,33 @@
+﻿# singleton DebugManager class
+extends Node
+
+## Input action used to toggle on and off the debug overlay
+const TOGGLE_ACTION := "debug_toggle"
+
+# child references
+@onready var debugCanvas: CanvasLayer = $DebugCanvas
+
+# export variables
+
+## Is debug functionality available?
+@export var _enable_debug: bool = true
+
+
+func debug_is_enabled() -> bool: return _enable_debug && (OS.has_feature("debug"))
+
+
+func _ready() -> void:
+	if (!debug_is_enabled()):
+		print("Debug mode disabled. Destroying Debug Menu")
+		debugCanvas.queue_free()
+		return
+
+
+func _input(event: InputEvent) -> void:
+	if debug_is_enabled() and event.is_action_pressed(TOGGLE_ACTION):
+		_toggle_debug_menu()
+
+
+func _toggle_debug_menu():
+	debugCanvas.visible = !debugCanvas.visible;
+	
