57bc2a1f0abf72bb15f68c3dfab6d51fa2c3b32a
Alex Hanlin <gluethulhu@gmail.com>
2024-11-24 16:35:11 -0600

    Fix performance bug and add clarity to DebugGameStackDisplay.gd

--- Debug/DebugGameStackDisplay.gd
+++ Debug/DebugGameStackDisplay.gd
@@ -1,3 +1,5 @@
+## Displays the game state stack in the debug menu
+
 extends Control
 class_name DebugGameStackDisplay
 
@@ -6,7 +8,7 @@ class_name DebugGameStackDisplay
 var stackCount: int = -1;
 var topState: GameState = null;
 
-# Inefficient, but should work. Can replace with signal listeners when appropriate
+# Inefficient, but works. Can replace with signal listeners when appropriate
 func _process(_delta):
 	_update_data()
 
@@ -15,14 +17,18 @@ func _update_data() -> void:
 	var stackChanged: bool = (stack.size() != stackCount || stack.front() != topState)
 	if (!stackChanged): return
 
+	stackCount = stack.size()
+	topState = stack.front()
+	
 	var displayString: String = ""
 
 	for state in stack:
 		displayString = _get_state_name(state) + '\n' + displayString
 
 	text.text = displayString
-	
-	
+
+
+## Retrieve name of state based on filename of its script
 func _get_state_name(state: GameState) -> String:
 	var filename : String = state.get_script().resource_path.get_file()
 	return filename.trim_suffix(".gd") # remove script suffix
